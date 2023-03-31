e4933087efb5d7ff71910a6c460394191cf3eaf7
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2023-03-31 11:52:51 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 209 ms, Memory: 60,544 KB -BaekjoonHub

---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.py
@@ -0,0 +1,39 @@
+rd = (-1, 1)
+ld = (-1, -1)
+lu = (1, -1)
+ru = (1, 1)
+
+
+def bruteforce(size):
+    for move in range(size - 1, 1, -1):
+        for x in range(size - move):
+            for y in range(1, size - 1):
+                for left in range(max(move - (size - y - 1), 1), min(y + 1, move)):
+                    right = move - left
+                    path = {cafes[x][y]}
+                    a, b = x, y
+                    for _ in range(right):
+                        a += 1
+                        b += 1
+                        path.add(cafes[a][b])
+                    for _ in range(left):
+                        a += 1
+                        b -= 1
+                        path.add(cafes[a][b])
+                    for _ in range(right):
+                        a -= 1
+                        b -= 1
+                        path.add(cafes[a][b])
+                    for _ in range(left - 1):
+                        a -= 1
+                        b += 1
+                        path.add(cafes[a][b])
+                    if len(path) == 2 * move:
+                        return 2 * move
+    return -1
+
+
+for tc in range(1, int(input()) + 1):
+    n = int(input())
+    cafes = [list(map(int, input().split())) for _ in range(n)]
+    print(f'#{tc}', bruteforce(n))
