1743c262324f2e445d877ff4ef9cc9c81883d4e2
CodingOnionFarmer <122241281+CodingOnionFarmer@users.noreply.github.com>
2023-04-06 02:04:01 +0900

    [Ruby V] Title: 친구, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Ruby/10075. 친구/친구.py
+++ 백준/Ruby/10075. 친구/친구.py
@@ -1,3 +1,17 @@
 n = int(input())
-rely = list(map(int, input().split()))
-print(sum(rely))
+original = list(map(int, input().split()))
+# with i , without i
+score = [[i, 0] for i in original]
+ho_pro = list(map(int, input().split()))
+for i in range(n - 1, 0, -1):
+    ho, pro = ho_pro[2 * i - 2], ho_pro[2 * i - 1]
+    if not pro:
+        score[ho][0] += score[i][1]
+        score[ho][1] += score[i][0]
+    elif pro == 1:
+        score[ho][0] += score[i][0]
+        score[ho][1] += score[i][1]
+    else:
+        score[ho][0] = max(score[ho][0], score[i][0])
+        score[ho][1] = max(score[ho][1], score[i][1])
+print(max(score[0]))
